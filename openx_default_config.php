658d59bc0d20456f0381fc3a7b92544b7a170a58
presedo <presedo@735d13b6-9817-0410-8766-e36946ffe9aa>
2009-10-13 14:13:35 +0000

    V1, with doc
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/openx/trunk@25356 735d13b6-9817-0410-8766-e36946ffe9aa

---
+++ openx_default_config.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * Openx default configuration
+ * must match with the openx server config
+ */
+$TYPO3_CONF_VARS['EXTCONF']['openx'] = array(
+
+	// Folders (need a slash at the beginning)
+	'OpenxFolders'	=> array(
+	
+		'Delivery'			=>'/www/jedelivre',
+		'Images'			=>'/www/mesimages'),
+		
+	// Files
+	'OpenxDeliveryFiles' => array(
+	
+		'AdClick'		=>'ox-ck.php',
+		'AdConversionTable'	=>'ox-tv.php',
+		'AdContent'		=>'ox-ac.php',
+		'AdConversion'		=>'ox-ti.php',
+		'AdConversionJs'	=>'ox-tjs.php',
+		'AdFrame'		=>'ox-afr.php',
+		'AdImage'		=>'ox-ai.php',
+		'AdJs'			=>'ox-ajs.php',
+		'AdLayer'		=>'ox-al.php',
+		'AdLog'			=>'ox-lg.php',
+		'AdPopup'		=>'ox-apu.php',
+		'AdView'		=>'ox-avw.php',
+		'XMLRPC'		=>'ox-axmlrpc.php',
+		'LocalInvocation'	=>'ox-alocal.php',
+		'FrontController'	=>'ox-fc.php',
+		'FlashInclude'		=>'ox-fl.js'
+		
+	)
+);
+
+?>
